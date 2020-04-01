fb0f64b47a5854131d9e2dbd887dea4ffec34d61
kimono <ljt@hust.edu.cn>
2020-04-01 17:28:23 +0800

    pa1-5 latter part finished and simply test

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -10,11 +10,13 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-	/* Test Part */
-	bool success;
-	char *e = "0xc0100000+  ($eax +5)*4 - *( $ebp + 8) + number";
-	expr(e, &success);
-	/* Test Part */
+	/* pa1-5 Test Part */
+	bool success = true;
+	//char *e = "0xc0100000+  ($eax +500)*4 - *( $ebp + 8) + number";
+	//char *e = "0x11111111111111111111111111111111111111111111111111";
+	char *e = "12315 + 0xa234 *  6- 123";
+	printf("Test expression:\n%s <==> %d\n", e, expr(e, &success));
+	/* pa1-5 Test Part */
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,6 +5,7 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
 
 enum {
   TK_NOTYPE = 256, TK_EQ,
@@ -49,9 +50,9 @@ static struct rule {
   {"0[xX][0-9a-fA-F]+", HEX},	// hex
   {"\\(", TK_LP},		// left parenthesis
   {"\\)", TK_RP},		// right parenthesis
-  {"[1-9]\\d*|0", NUM},		// number
-  {"[a-zA-Z_]+\\w*", VAR},	// variable
-  {"\\$[eE][a-zA-Z]{2}", REG},	// register
+  {"[1-9][0-9]*|0", NUM},		// number
+  {"[a-zA-Z_]\\w*", VAR},	// variable
+  {"\\$[eE][a-zA-Z]{2}|pc", REG},	// register
   {"!=", TK_NEQ},		// not equal
   {"!", TK_NOT},		// not
   {"&&", TK_AND},		// and
@@ -142,7 +143,7 @@ static bool make_token(char *e) {
   return true;
 }
 
-/* pa1-5 test
+/* pa1-5 test (for make_token)
  * FOR TEST: output the information of token in tokenArray
  */
 void tokenOutput() {
@@ -157,16 +158,216 @@ void tokenOutput() {
 }
 /* pa1-5 test */
 
+/* pa1-5 */
+static bool check_parentheses(int p, int q, bool *success) {
+	int i, st = 0;
+	bool bnf = true;
+	if (tokens[p].type != TK_LP || tokens[q].type != TK_RP) {
+		bnf = false;
+	}
+	for (i = p + 1; i < q; i++) {
+		if (tokens[i].type == TK_LP) {
+			st++;
+		}
+		else if (tokens[i].type == TK_RP) {
+			st--;
+			if (st < 0) {
+				bnf = false;
+			}
+		}
+	}
+	if (tokens[p].type == TK_LP) {
+		st++;
+	}
+	if (tokens[q].type == TK_RP) {
+		st--;
+	}
+	if (st != 0) {
+		*success = false;
+		return false;
+	}
+	return bnf;
+}
+
+static bool is_op(int p) {
+	if (tokens[p].type == HEX || tokens[p].type == NUM || tokens[p].type == VAR || tokens[p].type == REG || tokens[p].type == TK_LP || tokens[p].type == TK_RP) {
+		return false;
+	}
+	return true;
+}
+
+static int op_priority(int p) {
+	int ret;
+	switch (tokens[p].type) {
+		case TK_OR:
+		case TK_AND:
+			ret = 0;
+			break;
+		case TK_EQ:
+		case TK_NEQ:
+			ret = 1;
+			break;
+		case TK_ADD:
+		case TK_SUB:
+			ret = 2;
+			break;
+		case TK_MUL:
+		case TK_DIV:
+			ret = 3;
+			break;
+		case DEREF:
+		case TK_NOT:
+			ret = 4;
+			break;
+		default:
+			ret = 5;
+	}
+	return ret;
+}
+
+static int main_op_position(int p, int q) {
+	int i;
+	int main_pos = p;
+	for (i = p; i < q; i++) {
+		if (is_op(i)) {
+			if (op_priority(i) <= op_priority(main_pos)) {
+				main_pos = i;
+			}
+		}
+		else if(tokens[i].type == TK_LP) {
+			while (tokens[i].type != ')' && i < q) {
+				i++;
+			}
+		}
+	}
+	return main_pos;
+}
+
+/* Evaluate the expression
+ * p -> token start, q -> token end, success -> process status
+ */
+static uint32_t eval(int p, int q, bool *success) {
+	if (!(*success)) {
+		printf("Invalid expression, Abort at [%d, %d].\n", p, q);
+		return 0;
+	}
+	if (p > q) {
+		/* Bad expression */
+		printf("Bad expression!, [%d, %d]\n", p, q);
+		*success = false;
+		return 0;
+	}
+	else if (p == q) {
+		/* single token */
+		switch (tokens[p].type) {
+			case NUM:
+			case HEX:
+				return strtoul(tokens[p].str, NULL, 0);
+			case VAR:
+				TODO();
+				return 0;
+			case REG:
+				if (strcmp(tokens[p].str + 1, "pc") == 0) {
+					return cpu.pc;
+				}
+				int i;
+				for (i = R_EAX; i <= R_EDI; i ++) { 
+					if (strcmp(tokens[p].str + 1, reg_name(i, 4)) == 0) {
+						return reg_l(i);
+					}
+				}
+				printf("Invalid register name: %s\n", tokens[p].str);
+				*success = false;
+				return 0;
+			default:
+				printf("Invalid expression, token[%d] is redundant.\n", p);
+				*success = false;
+				return 0;
+		}
+	}
+	else if (check_parentheses(p, q, success)) {
+		/* The expression is surrounded by a matched () */
+		return eval(p + 1, q - 1, success);
+	}
+	else {
+		if (!(*success)) {
+			return 0;
+		}
+		int op_pos = main_op_position(p, q);
+		uint32_t val1 = 0;
+		if (tokens[op_pos].type != DEREF && tokens[op_pos].type != TK_NOT) {
+			val1 = eval(p, op_pos - 1, success);
+		}
+		if (!(*success)) {
+			return 0;
+		}
+		uint32_t val2 = eval(op_pos + 1, q, success);
+		if (!(*success)) {
+			return 0;
+		}
+		switch (tokens[op_pos].type) {
+			case TK_ADD:
+				return val1 + val2;
+			case TK_SUB:
+				if (val1 < val2) {
+					printf("Negative result occurs in calculation (%d - %d), abort.\n", val1, val2);
+					*success = false;
+					return 0;
+				}
+				return val1 - val2;
+			case TK_MUL:
+				return val1 * val2;
+			case TK_DIV:
+				if (val2 == 0) {
+					printf("Cannot divide 0 (%d / %d)\n", val1, val2);
+					*success = 0;
+					return 0;
+				}
+				return val1 / val2;
+			case TK_EQ:
+				return val1 == val2;
+			case TK_NEQ:
+				return val1 != val2;
+			case TK_AND:
+				return val1 && val2;
+			case TK_OR:
+				return val1 || val2;
+			case TK_NOT:
+				return !val2;
+			case DEREF:
+				return vaddr_read(val2, 1);
+			default:
+				printf("main operator goes wrong: %d\n", tokens[op_pos].type);
+				*success = false;
+				return 0;
+		}
+	}
+}
+
+static inline void check_deref() {
+	int i;
+	for (i = 0; i < nr_token; i++) {
+		if (tokens[i].type == '*' && (i == 0 || is_op(i - 1))) {
+			tokens[i].type = DEREF;
+		}
+	}
+}
+/* pa1-5 */
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
-  tokenOutput();
+  /* pa1-5 test */
+  //tokenOutput();
+  /* pa1-5 test */
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  /* pa1-5 */
+  check_deref();
 
-  return 0;
+  return eval(0, nr_token-1, success);
+  /* pa1-5 */
 }
