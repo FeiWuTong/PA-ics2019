67e71695120d00ca2e4825276efd235ae88c0429
kimono <ljt@hust.edu.cn>
2020-07-15 13:47:10 +0800

    pa2-3 finished, test all case (including two str.c that need to implement klib in nexus-am), get basic TRM

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,18 +4,51 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
+  assert(0);
   return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  int n = 0, num, buf_n;
+  char buf[10];
+  char *s;
+  while (*fmt) {
+	if (*fmt == '%') {
+	  switch (*++fmt) {
+		case 'd':
+		  num = va_arg(ap, int), buf_n = 0;
+		  while (num) {
+			buf[buf_n++] = num % 10 + '0';
+			num /= 10;
+		  }
+		  while (buf_n--) out[n++] = buf[buf_n];
+		  break;
+		case 's':
+		  s = va_arg(ap, char*);
+		  while (*s) out[n++] = *s++;
+		  break;
+		default:
+		  out[n++] = *fmt;
+	  }
+	} else {
+		out[n++] = *fmt;
+	}
+	fmt++;
+  }
+  out[n] = '\0';
+  return n;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+  int n = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return n;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
+  assert(0);
   return 0;
 }
 
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,67 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t n = 0;
+  if (s != NULL) {
+	  while (s[n] != '\0') n++;
+  }
+  return n;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  assert(dst != NULL && src != NULL);
+  int i, n2 = strlen(src);
+  if (dst <= src) {
+	for (i = 0; i <= n2; i++) dst[i] = src[i];
+  } else {
+	for (i = n2; i >= 0; i--) dst[i] = src[i];
+  }
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
+  assert(0);
   return NULL;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  assert(dst != NULL && src != NULL);
+  int n1 = strlen(dst);
+  while (*src != '\0') dst[n1++] = *(src++);
+  dst[n1] = '\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  if (s1 == NULL || s2 == NULL) return s1 != NULL ? 1 : (s2 == NULL ? 0 : -1);
+  while (*s1 != '\0' && *s1 == *s2) s1++, s2++;
+  return *s1 - *s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  assert(0);
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  int i;
+  for (i = 0; i < n; i++) {
+	*(int *)(v + i) = c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+  assert(0);
   return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  if (s1 == NULL || s2 == NULL) return s1 != NULL ? 1 : (s2 == NULL ? 0 : -1);
+  int i;
+  for (i = 0; i < n; i++) {
+	if (*(int *)(s1 + i) != *(int *)(s2 + i)) return *(int *)s1 - *(int *)s2;
+  }
   return 0;
 }
 
