3ed5aaaafe3ef04fb42abad6cb36b0629d23a02a
kimono <ljt@hust.edu.cn>
2020-07-27 22:35:20 +0800

    pa3-3 part1: loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,25 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr eh;
+  Elf_Phdr ph;
+  int i;
+  size_t phoff;
+  ramdisk_read(&eh, 0, sizeof(Elf_Ehdr));
+
+  for (i = 0, phoff = eh.e_phoff; i < eh.e_phnum; i++) {
+	phoff += ramdisk_read(&ph, phoff, eh.e_phentsize);
+	if (ph.p_type == PT_LOAD) {
+	  ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+	  if (ph.p_memsz > ph.p_filesz) memset((void *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
+	} else {
+	  break;
+	}
+  }
+  return eh.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
